6dfb1c2b9b91d2ce20cfe8ada7f475e905c99854
Natalia Khodorova <icxodnik@gmail.com>
2023-12-24 13:03:50 +0100

    implement the first task

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -1,4 +1,52 @@
 import { galleryItems } from './gallery-items.js';
 // Change code below this line
 
-console.log(galleryItems);
+const refGalleryContainer = document.querySelector(".gallery");
+refGalleryContainer.addEventListener("click", function onClick(e) {
+    e.preventDefault();
+    if (!e.target.classList.contains("gallery__image")) {
+        return;
+    }
+    showImage(e.target.dataset.source);
+});
+
+generateGalleryMarkup();
+
+function generateGalleryMarkup() {
+    let data = "";
+    for (let galleryItem of galleryItems) {
+        data += getItemElMarkup(galleryItem);
+    }
+    refGalleryContainer.insertAdjacentHTML("afterbegin", data);
+}
+
+function showImage(imgSource) {
+    const eventHandler = (e) => {
+        if (e.code != "Escape") { return; }
+        popup.close();
+    }
+
+    const popup = basicLightbox.create(`<img src="${imgSource}" />`, {
+        onShow() {
+            document.addEventListener("keydown", eventHandler);
+        },
+        onClose() {
+            document.removeEventListener("keydown", eventHandler);
+        }
+    });
+    popup.show();
+}
+
+function getItemElMarkup({ preview, original, description }) {
+    return (
+        `<li class="gallery__item">
+        <a class="gallery__link" href="${original}">
+        <img
+            class="gallery__image"
+            src="${preview}"
+            data-source="${original}"
+            alt="${description}"
+        />
+        </a>
+    </li>`)
+}
