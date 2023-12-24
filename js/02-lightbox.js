2fb9e95a5734e60c6782920caa09e2a94ad145ec
Natalia Khodorova <icxodnik@gmail.com>
2023-12-24 13:34:46 +0100

    implement the second task

--- js/02-lightbox.js
+++ js/02-lightbox.js
@@ -1,4 +1,31 @@
 import { galleryItems } from './gallery-items.js';
 // Change code below this line
+const refGalleryContainer = document.querySelector(".gallery");
+generateGalleryMarkup();
+// var gallery = refGalleryContainer.simpleLightbox();
+var lightbox = new SimpleLightbox('.gallery a', {
+    captions: true,
+    captionDelay: 250,
+    captionType: 'attr',
+    captionsData: 'alt'
+});
 
-console.log(galleryItems);
+
+function generateGalleryMarkup() {
+    let data = galleryItems.map(getItemElMarkup).join("");
+    refGalleryContainer.insertAdjacentHTML("afterbegin", data);
+}
+
+function getItemElMarkup({ preview, original, description }) {
+    return (
+        `<li class="gallery__item">
+        <a class="gallery__link" href="${original}">
+            <img
+                class="gallery__image"
+                src="${preview}"
+                data-source="${original}"
+                alt="${description}"
+            />
+        </a>
+    </li>`)
+}
